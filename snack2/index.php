73e7cb69f87819da2d9cf662cf4446639fc11e2a
simla04 <jessicasowi@gmail.com>
2022-12-13 13:18:48 +0100

    .

--- snack2/index.php
+++ snack2/index.php
@@ -2,7 +2,6 @@
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 -->
 
-
 <?php
 $nome =$_GET['nome'];
 $email = $_GET['email'];
@@ -15,7 +14,7 @@ $eta = $_GET['eta'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack2</title>
 </head>
 
 <body>
